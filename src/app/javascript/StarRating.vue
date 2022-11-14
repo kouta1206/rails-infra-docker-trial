4b29b775f5cbcb849d8c6e897d148952992e2dd5
kouta1206 <ko.bump1206@icloud.com>
2022-11-14 17:48:51 +0900

    star-ratingコンポーネント作成

---
+++ src/app/javascript/StarRating.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <input type="hidden" name="movie[evaluation]" :value="rating" />
+    <StarRating v-model="rating"></StarRating>
+  </div>
+</template>
+
+<script>
+import { StarRating } from 'vue-rate-it';
+export default {
+  components: {
+    StarRating
+  },
+  data() {
+    return {
+      rating: 3,
+    };
+  },
+};
+</script>
+
+<style>
+
+</style>
