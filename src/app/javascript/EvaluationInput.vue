624f76a8aee6731f1b2f9d469d0a4e7574293e25
kouta1206 <ko.bump1206@icloud.com>
2022-11-15 01:00:39 +0900

    コンポーネント修正

--- src/app/javascript/EvaluationInput.vue
+++ src/app/javascript/EvaluationInput.vue
@@ -1,35 +1,19 @@
 <template>
   <div>
-    <input type="hidden" name="movie[evaluation]" :value="puzzleSelect" />
-    <star-rating v-model="puzzleSelect"></star-rating>
+    <input type="hidden" name="movie[evaluation]" :value="rating" />
+    <star-rating v-model="rating"></star-rating>
   </div>
 </template>
 
 <script type="text/javascript">
 import { StarRating } from "vue-rate-it";
-import { PuzzlePiece } from "vue-rate-it/glyphs/puzzle-piece";
 export default {
-  created() {
-    this.settings = {
-      itemSize: 20,
-      incrementer: 0.5,
-      colorz: "#ddfbcc",
-      puzzlePiece: PuzzlePiece,
-    };
-  },
   components: {
     StarRating,
-    PuzzlePiece,
   },
   data() {
     return {
-      puzzleSelect: 2,
-      settings: {
-        itemSize: 20,
-        incrementer: 0.5,
-        colorz: "#ddfbcc",
-        puzzlePiece: "",
-      },
+      rating: 3,
     };
   },
 };
