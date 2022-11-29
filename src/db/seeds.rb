08155fc82de76abdb520f0da933461f00b0d8fd7
kouta1206 <ko.bump1206@icloud.com>
2022-11-29 22:33:00 +0900

    シーダの作成途中

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+binding.pry
+agent = Mechanize.new
+agent.redirect_ok = false
+agent.user_agent_alias = "Windows Mozilla"
+page = agent.get('https://eiga.com/movie/review/ranking/')
+titles = page.search("h2.title  a")
+reviews = page.search(".txt-box ul li:first-child a")
+# review_txt = reviews.map {|review|  reviews.inner_text }
+review_txt = reviews.inner_text
+# raing_stars()
+# image_path()
+# texts = titles.map {|title|  title.inner_text }
+# texts.slice!(10..12)
+p review_txt
